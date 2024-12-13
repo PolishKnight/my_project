a2ba23bf2ed01bcde5e9f51c0e485e6294ea0ab8
Mateusz Lewandowski <mati13042007@wp.pl>
2024-12-13 11:30:57 +0000

    buy

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -6,6 +6,6 @@ fn greet(name: String) -> String {
 }
 
 #[ic_cdk::query]
-fn calculate_currency_price(curr_a: Nat, curr_b: Nat) -> Nat{
-    curr_a / curr_b
+fn calculate_currency_price(curr_a: Nat, curr_b: Nat) -> Nat {
+    curr_a * curr_b
 }
--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -1,14 +1,28 @@
 <script setup>
 import { ref } from 'vue';
 import { my_project_backend } from 'declarations/my_project_backend/index';
+
 const rates = ref([]);
+const iloscWaluty = ref([])
 
 const getDataFromNBP =  async () => {
   const res = await fetch("https://api.nbp.pl/api/exchangerates/tables/A/?format=json");
   const jsonData = await res.json();
   rates.value = jsonData[0].rates;
-  console.log(rates.value);
+  // console.log(rates.value);
+  iloscWaluty.value = jsonData[0].rates.map(() => 0);
 }
+
+const kupWalute = async (index) => {
+  console.log(rates.value[index]);
+  const ilosc = BigInt(iloscWaluty.value);
+  const cena = BigInt(rates.value[index].mid * 10e16);
+  console.log(ilosc, cena);
+
+  const koszt = await my_project_backend.calculate_currency_price(ilosc, cena);
+  console.log(koszt / BigInt(10e16));
+}
+
 getDataFromNBP();
 
 </script>
@@ -24,11 +38,15 @@ getDataFromNBP();
         <th>Nazwa waluty</th>
         <th>Kod waluty</th>
         <th>Cena</th>
+        <th>Ilość waluty</th>
+        <th></th>
       </tr>
-      <tr v-for="rate in rates" >
+      <tr v-for="(rate, index) in rates">
         <td>{{ rate.currency }}</td>
         <td>{{ rate.code }}</td>
         <td>{{ rate.mid }}</td>
+        <td><input type="number" v-model="iloscWaluty" /></td>
+        <td><button @click="kupWalute(index)">Kup</button></td>
       </tr>
     </table>
   </main>
