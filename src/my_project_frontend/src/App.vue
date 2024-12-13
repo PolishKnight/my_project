8ded85f04ab9b74aa1dfd178f7ea5a394815b474
Mateusz Lewandowski <mati13042007@wp.pl>
2024-12-13 10:48:40 +0000

    update rs, did, table vue

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
 import { ref } from 'vue';
 import { my_project_backend } from 'declarations/my_project_backend/index';
-let input = ref('');
+const rates = ref([]);
 
 const getDataFromNBP =  async () => {
   const res = await fetch("https://api.nbp.pl/api/exchangerates/tables/A/?format=json");
   const jsonData = await res.json();
-  const rates = jsonData[0].rates;
-  console.log(rates);
+  rates.value = jsonData[0].rates;
+  console.log(rates.value);
 }
 getDataFromNBP();
 
@@ -18,5 +18,18 @@ getDataFromNBP();
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
+
+    <table>
+      <tr>
+        <th>Nazwa waluty</th>
+        <th>Kod waluty</th>
+        <th>Cena</th>
+      </tr>
+      <tr v-for="rate in rates" >
+        <td>{{ rate.currency }}</td>
+        <td>{{ rate.code }}</td>
+        <td>{{ rate.mid }}</td>
+      </tr>
+    </table>
   </main>
 </template>
